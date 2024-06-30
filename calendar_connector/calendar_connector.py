ad08067f891a2bad85ea350f406c93b482920bcd
otaviopl <otavio.pereira.lopes@gmail.com>
2024-06-30 00:10:56 -0300

    FEAT :sparkles: new function get_events-calendarAPI- | CHORE: rename calendarTeste file

--- calendar_connector/calendar_connector.py
+++ calendar_connector/calendar_connector.py
@@ -7,4 +7,29 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SCOPES = ['']
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+SERVICE_ACCOUNT_FILE = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
+
+credentials = service_account.Credentials.from_service_account_file(
+        SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+
+service = build('calendar', 'v3', credentials=credentials)
+
+NOTION_API_KEY = os.getenv('NOTION_API_KEY')
+DATABASE_ID = os.getenv('NOTION_DATABASE_ID')
+NOTION_API_URL = "https://api.notion.com/v1/pages"
+
+headers = {
+    "Authorization": f"Bearer {NOTION_API_KEY}",
+    "Content-Type": "application/json",
+    "Notion-Version": "2022-06-28"
+}
+
+def get_events():
+    now = datetime.datetime.utcnow().isoformat() + 'Z'
+    events_result = service.events().list(calendarId='primary', timeMin=now,
+                                        maxResults=10, singleEvents=True,
+                                        orderBy='startTime').execute()
+    events = events_result.get('items', [])
+
+    return events
