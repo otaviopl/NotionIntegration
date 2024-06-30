041388a571c1c37c6d66736d8ea4f11828bca1de
otaviopl <otavio.pereira.lopes@gmail.com>
2024-06-30 12:09:16 -0300

    FIX: fix using the correct method of auth at calendar api

--- calendar_connector/calendar_connector.py
+++ calendar_connector/calendar_connector.py
@@ -19,43 +19,44 @@ SERVICE_ACCOUNT_FILE = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
 NOTION_API_KEY = os.getenv('NOTION_API_KEY')
 DATABASE_ID = os.getenv('NOTION_DATABASE_ID')
 NOTION_API_URL = "https://api.notion.com/v1/pages"
+API_KEY=os.getenv('API_KEY')
 
 headers = {
-    "Authorization": f"Bearer {NOTION_API_KEY}",
-    "Content-Type": "application/json",
-    "Notion-Version": "2022-06-28"
+	"Authorization": f"Bearer {NOTION_API_KEY}",
+	"Content-Type": "application/json",
+	"Notion-Version": "2022-06-28"
 }
 
 def build_google_service():
-    """Google Calendar service builder."""
-    credentials = service_account.Credentials.from_service_account_file(
-        SERVICE_ACCOUNT_FILE, scopes=SCOPES
-    )
-    service = build('calendar', 'v3', credentials=credentials)
-    return service
+	"""Google Calendar service builder."""
+	credentials = service_account.Credentials.from_service_account_file(
+		SERVICE_ACCOUNT_FILE, scopes=SCOPES
+	)
+	service = build('calendar', 'v3', developerKey=API_KEY)
+	return service
 
 def get_events(service=None):
-    """Get Events from Google Calendar.
+	"""Get Events from Google Calendar.
 
-    Args:
-        service: Service from Google API.
+	Args:
+		service: Service from Google API.
 
-    Returns:
-        Events List.
-    """
-    if service is None:
-        print('criando servico com build google service\n')
-        service = build_google_service()
-    
-    now = datetime.datetime.utcnow().isoformat() + 'Z'
-    events_result = service.events().list(
-        calendarId='primary',
-        timeMin=now,
-        maxResults=10,
-        singleEvents=True,
-        orderBy='startTime'
-    ).execute()
-    events = events_result.get('items', [])
-    print('eventos resgatados',events)
+	Returns:
+		Events List.
+	"""
+	if service is None:
+		print('criando servico com build google service\n')
+		service = build_google_service()
+	
+	now = datetime.datetime.utcnow().isoformat() + 'Z'
+	events_result = service.events().list(
+			calendarId='primary',
+		timeMin=now,
+		maxResults=10,
+		singleEvents=True,
+		orderBy='startTime'
+	).execute()
+	events = events_result.get('items', [])
+	print('eventos resgatados',events)
 
-    return events
+	return events
